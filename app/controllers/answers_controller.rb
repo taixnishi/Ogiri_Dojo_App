26e55c2567322f8637e06b59d428551464988200
Taichi Nishikawa <taichi.1999.baske@gmail.com>
2020-08-16 17:46:09 +0900

    modify cloudinary

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,30 +1,39 @@
 class AnswersController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_find
 
   def create
-    theme = Theme.find(params[:theme_id])
-    # @answer = theme.answers.create(answer_params)
-    @answer = theme.answers.new(answer_params)
+    @answer = @theme.answers.new(answer_params)
     @answer.user_id = current_user.id
     if @answer.save
       flash[:success] = "コメントしました"
       redirect_back(fallback_location: root_path)
     else
-      flash[:success] = "コメントできませんでした"
+      flash[:notice] = "コメントできませんでした"
       redirect_back(fallback_location: root_path)
     end
   end
 
   def destroy
-    @theme = Theme.find(params[:theme_id])
     @answer = @theme.answers.find(params[:id])
     @answer.destroy
     redirect_back(fallback_location: root_path)
   end
 
   private
+    def set_find
+      @theme = Theme.find(params[:theme_id])
+    end
 
     def answer_params
       params.require(:answer).permit(:answer)
     end
 end
+
+
+
+
+
+
+
+
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,5 @@
 class FavoritesController < ApplicationController
   def create
-    # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
-    # つまり、favorite.user_id = current_user.idが済んだ状態で生成されている。
-    # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
     favorite = current_user.favorites.build(answer_id: params[:answer_id],theme_id: params[:theme_id])
     favorite.save
     redirect_back(fallback_location: root_path)
