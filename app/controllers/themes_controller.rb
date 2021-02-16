6aa68ee77aa5e266237ca5c5ed6aff5eb650e7e9
taixnishi <taichi.1999.baske@gmail.com>
2021-02-17 03:27:37 +0900

    testuserを追加+ログインなしでもindex見れるように変更

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,5 +1,5 @@
 class ThemesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! ,except: [:index]  
 
   def index
     @themes = Theme.all.order(created_at: :desc)
